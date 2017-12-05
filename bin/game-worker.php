437c94a984e2f91e13f97f2dddcde04943169d1a
samizdam <samizdam@inbox.ru>
2017-12-05 23:24:37 +0300

    Prepare entry script.

--- bin/game-worker.php
+++ bin/game-worker.php
@@ -2,6 +2,17 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// params getting
+$players = [];
+foreach (json_decode($argv[1]) as $playerId) {
+    $players[$playerId] = new \Hexammon\GameWorker\Player();
+}
+
+$boardParams = json_decode($argv[2]);
+$boardConfig = new \Hexammon\GameWorker\BoardConfig(...$boardParams);
+$ruleSet = new \Hexammon\HexoNards\Game\Rules\ClassicRuleSet();
+
+// Application logic
 $game = (new \Hexammon\GameWorker\GameBuilder())->build($players, $boardConfig, $ruleSet);
 $application = new \Hexammon\GameWorker\Application($game);
 $gameUUID = \Lootils\Uuid\Uuid::createV4();
@@ -11,11 +22,17 @@ $router = new \Hexammon\GameWorker\Router($application, $gameUUID);
 
 $client->on('open', function (\Thruway\ClientSession $session) use ($router) {
     $router->bindActions($session);
+
 });
 
+
+// Transport layer
 try {
+    $client->addTransportProvider(new \Thruway\Transport\PawlTransportProvider());
     $client->start();
 } catch (Exception $e) {
-    echo 'Cannot start thruway client. ';
+    echo 'Cannot start thruway client. '. PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+    echo $e->getTraceAsString();
     exit(1);
 }
